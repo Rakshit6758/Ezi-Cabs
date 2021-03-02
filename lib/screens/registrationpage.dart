6cc58c55b0968b3aedb313c11eb8cd368b6a13da
Rakshit6758 <500067642@stu.upes.ac.in>
2021-03-02 13:11:46 +0530

    New User Registration Complete

--- lib/screens/registrationpage.dart
+++ lib/screens/registrationpage.dart
@@ -1,35 +1,94 @@
+import 'package:connectivity/connectivity.dart';
 import 'package:ezi_cabs/brand_colors.dart';
 import 'package:ezi_cabs/screens/loginpage.dart';
 import 'package:ezi_cabs/widgets/TaxiButton.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:ezi_cabs/screens/mainpage.dart';
 
-class RegistrationPage extends StatelessWidget {
+class RegistrationPage extends StatefulWidget {
 
   static const String id = 'register';
+
+  @override
+  _RegistrationPageState createState() => _RegistrationPageState();
+}
+
+class _RegistrationPageState extends State<RegistrationPage> {
+  final GlobalKey<ScaffoldState> scaffoldkey = new GlobalKey<ScaffoldState>();
+
+  void showSnackBar(String title){
+    final snackbar = SnackBar(
+      content: Text(title, textAlign: TextAlign.center, style: TextStyle(fontSize: 15),),
+    );
+    scaffoldkey.currentState.showSnackBar(snackbar);
+  }
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   var fullNameController = TextEditingController();
+
   var emailController = TextEditingController();
+
   var phoneController = TextEditingController();
+
   var passwordController = TextEditingController();
 
   void registerUser() async{
-    final User user = (await _auth.createUserWithEmailAndPassword(
-        email: 'an email',
-        password: 'a password',
-    )).user;
+    final User user =(await _auth.createUserWithEmailAndPassword(
+        email: emailController.text ,
+        password: passwordController.text,
+    ).catchError((ex){
+      // check error and display message
+      PlatformException thisEx = ex;
+      showSnackBar(thisEx.message);
+
+
+    })).user;
 
     if(user != null){
-      print('registration successful');
+      DatabaseReference newUserRef = FirebaseDatabase.instance.reference().child('users/${user.uid}');
+
+      // Prepare data to be saved
+      Map userMap = {
+        'fullname': fullNameController.text,
+        'email': emailController.text,
+        'phone': phoneController.text,
+      };
+
+      newUserRef.set(userMap);
+
+      Navigator.pushNamedAndRemoveUntil(context, MainPage.id, (route) => false);
     }
+
+
+    // try {
+    //   UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+    //       email: emailController.text,
+    //       password: passwordController.text,
+    //   );
+    // } on FirebaseAuthException catch (e) {
+    //   if (e.code == 'weak-password') {
+    //     print('The password provided is too weak.');
+    //   } else if (e.code == 'email-already-in-use') {
+    //     print('The account already exists for that email.');
+    //   }
+    // } catch (e) {
+    //   print(e);
+    // }
+    // User user = FirebaseAuth.instance.currentUser;
+    // if (!user.emailVerified) {
+    //   await user.sendEmailVerification();
+    // }
 
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldkey,
         backgroundColor: Colors.black,
         body: SafeArea(
           child: SingleChildScrollView(
@@ -143,7 +202,34 @@ class RegistrationPage extends StatelessWidget {
                           TaxiButton(
                             title: 'REGISTER',
                             color: BrandColors.colorMustard,
-                            onPressed:(){
+                            onPressed:() async{
+
+                              var connectivityResult = await Connectivity().checkConnectivity();
+                              if(connectivityResult != ConnectivityResult.mobile && connectivityResult != ConnectivityResult.wifi){
+                                showSnackBar('No Internet connectivity');
+                              }
+
+                              if(fullNameController.text.length <3){
+                                showSnackBar('Please provide a valid Full Name');
+                                return;
+                              }
+
+                              if(!emailController.text.contains('@')){
+                                showSnackBar('Please provide a valid email address');
+                                return;
+                              }
+
+                              if(phoneController.text.length < 10){
+                                showSnackBar('Please provide a valid phone number');
+                                return;
+                              }
+
+                              if(passwordController.text.length < 8){
+                                showSnackBar('password must be at least 8 characters ');
+                                return;
+                              }
+
+
                               registerUser();
                             },
                           )
